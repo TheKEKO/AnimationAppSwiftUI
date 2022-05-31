2f9159595ed803a724d7ff61a243b75f5d753295
Aleksandr F <a7214370@gmail.com>
2022-05-31 21:42:49 +0300

    Work with ContentView

--- AnimationAppSwiftUI/ContentView.swift
+++ AnimationAppSwiftUI/ContentView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var onOff = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Button(action: buttonAction) {
+                    VStack {
+                        VStack {
+                            Spacer()
+                            if onOff {
+                                Spacer()
+                                TextView()
+                                    .transition(.identity)
+                                LightView()
+                                    .transition(.identity)
+                                    .frame(width: 200, height: 250)
+                            }
+                        }
+                        Image(systemName: onOff ? "flashlight.on.fill" :
+                                "flashlight.off.fill")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 120, height: 170)
+                        
+                        Image(systemName: "power.circle.fill")
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(onOff ? .red : .green)
+                            .rotation3DEffect(.degrees(onOff ? 0 : 360), axis:
+                                                (x: 0, y: 1, z: 0))
+                            .animation(Animation.easeOut(duration: 0.7), value: onOff)
+                    }
+                }
+            }
+            .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
+        }
+    }
+    
+    private func buttonAction() {
+        withAnimation {
+            onOff.toggle()
+            SoundManager.instance.playSound()
+        }
     }
 }
 
