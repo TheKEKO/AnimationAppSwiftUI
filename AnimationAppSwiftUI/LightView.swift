8095440e1b350614d2eee04f08c0ab13a2e1f952
Aleksandr F <a7214370@gmail.com>
2022-05-31 19:43:10 +0300

    Setup LightView

---
+++ AnimationAppSwiftUI/LightView.swift
@@ -0,0 +1,23 @@
+//
+//  LightView.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Aleksandr F. on 30.05.2022.
+//
+
+import SwiftUI
+
+struct LightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct LightView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightView()
+            .frame(width: 200, height: 250)
+    }
+}
+
