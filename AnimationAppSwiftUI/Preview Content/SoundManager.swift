d2802c7e6e806eb6599b25f28f59f8130658b2b4
Aleksandr F <a7214370@gmail.com>
2022-05-31 21:43:44 +0300

    Work with SoundManager

--- AnimationAppSwiftUI/Preview Content/SoundManager.swift
+++ AnimationAppSwiftUI/Preview Content/SoundManager.swift
@@ -6,6 +6,24 @@
 //
 
 import Foundation
+import AVKit
 
+class SoundManager {
+
+    static let instance = SoundManager()
+
+    var player: AVAudioPlayer?
+
+    func playSound() {
+
+        guard let url = Bundle.main.url(forResource: "sound", withExtension: ".mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch _ {
+            print("")
+        }
+    }
+}
 
 
