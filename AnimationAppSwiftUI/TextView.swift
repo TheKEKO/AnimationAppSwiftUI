209ad7e7c9e8c8162d55cd1bed47da4940df4360
Aleksandr F <a7214370@gmail.com>
2022-05-31 19:42:40 +0300

    Setup TextView

---
+++ AnimationAppSwiftUI/TextView.swift
@@ -0,0 +1,21 @@
+//
+//  TextView.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Aleksandr F. on 31.05.2022.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
