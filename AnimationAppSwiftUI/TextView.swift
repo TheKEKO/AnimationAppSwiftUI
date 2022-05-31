19b9ff3ff1dcb028c5034615064ffc8c552b6be8
Aleksandr F <a7214370@gmail.com>
2022-05-31 21:41:49 +0300

    Work with TextView

--- AnimationAppSwiftUI/TextView.swift
+++ AnimationAppSwiftUI/TextView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct TextView: View {
+    @State var show = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            ZStack {
+                
+                Text("SwiftBook")
+                    .foregroundColor(Color.yellow.opacity(0.4))
+                    .font(.system(size: 50))
+                    .bold()
+                
+                Text("SwiftBook")
+                    .foregroundColor(.yellow)
+                    .font(.system(size: 50))
+                    .bold()
+                    .mask(
+                        
+                        Capsule()
+                            .fill(LinearGradient(gradient: .init(colors: [.clear,
+                                .yellow, .clear]), startPoint: .top, endPoint: .bottom))
+                            .rotationEffect(.init(degrees: 30))
+                            .offset(x: self.show ? 180 : -130)
+                    )
+            }
+        }
+        .onAppear {
+            withAnimation(Animation.default.speed(0.1).delay(0)
+                .repeatForever(autoreverses: false)) {
+                    self.show.toggle()
+                }
+        }
     }
 }
+
 
 
 struct TextView_Previews: PreviewProvider {
